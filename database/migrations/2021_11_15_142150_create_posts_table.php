81e929d05d0fa00531ae49339dea01b6a2f3474d
MascarBone <simone.bonelli@gmail.com>
2021-11-15 15:38:14 +0100

    first commit

---
+++ database/migrations/2021_11_15_142150_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('image_url');
+            $table->string('author', 50);
+            $table->text('content');
+            $table->date('post_date');
+            $table->string('slug', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
