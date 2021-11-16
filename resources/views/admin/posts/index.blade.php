8b1082faa7e7bc41c63d7cad1443f4d3aa756cfe
MascarBone <simone.bonelli@gmail.com>
2021-11-16 16:53:58 +0100

    models, controllers, and index of posts

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+    
+<div class="container">
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Post Title</th>
+                <th scope="col">Post Author</th>
+                <th scope="col">Post Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($posts as $post)
+                
+                <tr>
+                    <th scope="row">{{$post->title}}</th>
+                    <td>{{$post->author}}</td>
+                    <td>{{$post->post_date}}</td>
+                </tr>
+
+            @empty
+                
+                <tr>
+                    <td colspan="3">There are no posts available</td>
+                </tr>
+                
+            @endforelse
+        
+        </tbody>
+    </table>
+
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::namespace('Admin')
+->prefix('admin')
+->name('admin.')
+->group( function() {
+    
+    Route::get('home', 'HomeController@index')->name('home');
+    Route::resource('posts', PostController::class);
+});
