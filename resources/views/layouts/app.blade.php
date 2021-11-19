2a3d2b1054e33acdf782f4caf2e1ca1ba1de8acf
MascarBone <simone.bonelli@gmail.com>
2021-11-19 16:25:34 +0100

    javascript app.blade, remove defer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Boolpress') }}</title>
 
     <!-- Scripts -->
-    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -78,5 +78,7 @@
     </div>
     {{-- <script text="text/javascript" src="{{ asset('js/app.js')}}"></script> --}}
     @yield('script')
+    
+   
 </body>
 </html>
